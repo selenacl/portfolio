f70064467868e338eeabcc8a54f8c374e3e4443a
Selena Castrolopez <selena@Selenas-MacBook-Pro.local>
2019-08-26 12:31:28 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome! This site is currently under construction.</h1>
+      <p>Come back soon!</p>
     </div>
   );
 }
