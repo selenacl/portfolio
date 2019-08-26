7aadcc337c9feaf34c2dee5f2d070e3beb8cf2b7
Selena Castrolopez <selena@Selenas-MacBook-Pro.local>
2019-08-26 12:46:57 -0400

    Updates

---
+++ precache-manifest.70c000a07df3fcff394306ff0b6f8107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40090d1775ee56a31d1e3281e28b091",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9f0f01aa1ea91c9985e8",
+    "url": "/portfolio/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "ea5dcfe79d1d102c92d3",
+    "url": "/portfolio/static/js/2.43a7f072.chunk.js"
+  },
+  {
+    "revision": "9f0f01aa1ea91c9985e8",
+    "url": "/portfolio/static/js/main.8e22d8d8.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  }
+]);
